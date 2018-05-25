896fbdff3e00bf74b02cd2c5ebad0df937e9b222
muturi254 <petermuturi74@gmail.com>
2018-05-25 15:26:26 +0300

    add url to handle registration/signup and login

--- gang/urls.py
+++ gang/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
