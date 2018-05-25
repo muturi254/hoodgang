0314cae4d8d71563ac2795da5f62820fd61852d9
muturi254 <petermuturi74@gmail.com>
2018-05-25 14:58:53 +0300

    database configuration

--- hoodgang/settings.py
+++ hoodgang/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'hoodgang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hoodgang',
+        'PASSWORD' : 'Number12',
+        'USER':'ashorta',   
     }
 }
 
