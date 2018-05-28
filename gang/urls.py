3a9bfd49303e1f8d7bf95aafb10916927366e7af
muturi254 <petermuturi74@gmail.com>
2018-05-28 12:14:57 +0300

    urls for views

--- gang/urls.py
+++ gang/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import include, url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^(?P<user_id>[0-9]+)/profile/$', views.profile, name='profile'),
+    url(r'^(?P<user_id>[0-9]+)/edits/$', views.profile_edit, name='profile_edit'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
