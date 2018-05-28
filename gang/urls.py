75012819e96718d0909468cbcf9dc88bff9871e4
muturi254 <petermuturi74@gmail.com>
2018-05-28 19:57:09 +0300

    new views post and hood

--- gang/urls.py
+++ gang/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^home/$', views.home, name='home'),
     url(r'^(?P<user_id>[0-9]+)/profile/$', views.profile, name='profile'),
     url(r'^(?P<user_id>[0-9]+)/edits/$', views.profile_edit, name='profile_edit'),
+    url(r'^newhood/$', views.new_hood, name='newHood'),
+    url(r'^Post/$', views.hood_post, name='newPost'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', login_view.logout, {"next_page": '/'}),
 ]
