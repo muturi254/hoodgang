9f9a92fb9ea77aaa6b70d7094ac8f1330d008031
muturi254 <petermuturi74@gmail.com>
2018-05-28 14:27:50 +0300

    post model

--- gang/models.py
+++ gang/models.py
@@ -49,6 +49,6 @@ class Category(models.Model):
 
 class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
-    post_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
     post_body = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
